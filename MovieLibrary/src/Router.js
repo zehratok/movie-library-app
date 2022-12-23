d9f4a415139261dc2b07c27772cdaee71a97594a
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-12-24 00:48:00 +0300

    First commit

---
+++ MovieLibrary/src/Router.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const Router = () => {
+    return (
+        <View>
+            <Text>Router</Text>
+        </View>
+    );
+}
+export default Router;
